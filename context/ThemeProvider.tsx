4cb5406419f9e97ea4bc41bfbb2aa398fe1703bb
Abhishek <abhi@LAPTOP-2S6N9AUA>
2023-10-29 13:20:05 +0530

    theme provider

---
+++ context/ThemeProvider.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import React, { useState, createContext, useContext } from "react";
+
+interface ThemeContextType {
+  mode: string;
+  setMode: (mode: string) => void;
+}
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
+  const [mode, setMode] = useState("");
+
+  return (
+    <ThemeContext.Provider value={{ mode, setMode }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export default function useTheme() {
+  const context = useContext(ThemeContext);
+
+  if (context === undefined) {
+    throw new Error("useTheme must be used within a ThemeProvider");
+  }
+  return context;
+}
