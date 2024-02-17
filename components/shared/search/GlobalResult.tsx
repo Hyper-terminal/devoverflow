333946c9a15800d537d58c124b27aa15fd7fbcdf
abhi <abhisharma001001001@gmail.com>
2024-02-17 16:52:14 +0530

    global result component

--- components/shared/search/GlobalResult.tsx
+++ components/shared/search/GlobalResult.tsx
@@ -4,13 +4,10 @@ import { useEffect, useState } from "react";
 import RenderTag from "../RenderTag";
 import { GlobalSearchFilters } from "@/constants/filter";
 
-
-
 export default function GlobalResult() {
   const [results, setResults] = useState([]);
-  const [type, setType] = useState('');
+  const [type, setType] = useState("");
   const searchParams = useSearchParams();
-
 
   useEffect(() => {
     async function fetchData() {
@@ -26,15 +23,17 @@ export default function GlobalResult() {
   return (
     <div className="background-light800_darkgradient text-dark200_light800 absolute mt-1 flex min-h-[150px] w-full grow flex-col items-center gap-1 rounded-xl px-4 ">
       <div className="flex gap-5 my-6 flex-wrap items-center">
-
         <h3>Filters</h3>
 
-        {GlobalSearchFilters?.map(item => (
+        {GlobalSearchFilters?.map((item) => (
           <div key={item.name} onClickCapture={() => setType(item?.value)}>
-            <RenderTag className="background-light800_dark300" _id={item?.value} name={item?.name} />
+            <RenderTag
+              className="background-light800_dark300"
+              _id={item?.value}
+              name={item?.name}
+            />
           </div>
         ))}
-
       </div>
       <h3>Top Matches</h3>
       {results?.map((item: any, index) => (
